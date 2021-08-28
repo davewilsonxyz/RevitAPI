cbfe9e0e7ba8a878a1d697e4ea7968c23ee09f81
David Wilson <39122987+davewilsonxyz@users.noreply.github.com>
2021-08-28 21:58:06 +0100

    course progress

---
+++ Revit API - LinkedIn/MyRevitCommands/GetElementId.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace MyRevitCommands
+{
+    //This transaction is needed
+    [TransactionAttribute(TransactionMode.ReadOnly)]
+    public class GetElementId : IExternalCommand
+    {
+        //This is the minimum required for a IExternalCommand
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get UI document
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+
+            try
+            {
+                //Pick object
+                Reference pickedObj = uidoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element);
+
+
+                //Display element Id
+                if (pickedObj != null)
+                {
+                    TaskDialog.Show("Element Id", pickedObj.ElementId.ToString());
+                }
+
+                //Either return succeeded, cancelled or failed
+                return Result.Succeeded;
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Result.Failed;
+            }
+     
+        }
+    }
+}
